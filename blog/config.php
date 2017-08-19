88a8706c35d0c9776d4c1433d98856aadcf11825
Saul Florez <saulflorez.backend@gmail.com>
2017-08-18 20:07:32 -0400

    Iniciando el proyecto, construcción de un blog

---
+++ blog/config.php
@@ -0,0 +1,12 @@
+<?php
+	$dbHost = 'localhost';
+	$dbName = 'cursophp';
+	$dbUser = 'root';
+	$dbPass = 'root';
+
+	try{
+		$pdo = new PDO("mysql:host=$dbHost;dbname=$dbName;", "$dbUser", "$dbPass"); 
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(Exception $e){
+		echo $e->getMessage();
+	}
---
+++ blog/index.php
@@ -0,0 +1,54 @@
+<?php 
+	include_once 'config.php';
+
+	$query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+	$query->execute();
+
+	$blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blog with Platzi</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Blog Title</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8">
+				<?php
+
+				foreach ($blogPosts as $blogPost) {
+					echo '<div class="blog-post">';
+					echo '<h2>'. $blogPost['title'] .'</h2>';
+					echo '<p>Jan 1, 2020 by <a href="">Alex</a></p>';
+					echo '<div class="blos-post-image">';
+					echo '<img src="images/keyboard.jpg" alt="" class="img-fluid">';
+					echo '</div>';
+					echo '<div class="blog-post-content">';
+					echo $blogPost['content'];
+					echo '</div>';
+					echo '</div>';
+				}
+
+				?>
+			</div>
+			<div class="col-md-4">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, illum iste explicabo officiis. At nemo quia molestias hic, nulla, optio beatae, perspiciatis cum eveniet deserunt voluptatum enim praesentium reiciendis ducimus!
+			</div>
+		</div>
+		<div class="row">
+			<footer>
+				This is a footer
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
